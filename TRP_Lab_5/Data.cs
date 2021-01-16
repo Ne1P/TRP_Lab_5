3e084b787af16dce56d2cd608b5bb6338ff7c4c1
Petro.Nei.KN.2017 <Petro.Nei.KN.2017@lpnu.ua>
2021-01-16 19:56:07 +0200

    Добавьте файлы проекта.

---
+++ TRP_Lab_5/Data.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TRP_Lab_5
+{
+    class Data
+    {
+        public static int[,] ReadInput()
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\Petro\Desktop\Algoritm\TPR\Lab_5\TRP_Lab_5\TRP_Lab_5\input.txt");
+            int[,] num = new int[lines.Length, lines[0].Split(' ').Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Split(' ');
+                for (int j = 0; j < temp.Length; j++)
+                    num[i, j] = Convert.ToInt32(temp[j]);
+            }
+            // перевіримо вивівши дані на результат
+            Console.WriteLine("Вхідні дані:");
+            for (int i = 0; i < num.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < num.GetLength(1); j++)
+                    Console.Write(num[i, j] + "\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            return num;
+
+        }
+    }
+}
---
+++ TRP_Lab_5/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TRP_Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int[,] input=Data.ReadInput();
+
+            searchSaddlePoint(input);
+
+            Console.ReadLine();
+        }
+
+        public static void searchSaddlePoint(int[,] input)
+        {
+            int[] minA = new int[5];
+            int[] maxB = new int[5];
+
+            int temp =99;
+
+
+            for (int i = 0; i < minA.Length; i++)
+            {
+                for (int j = 0; j < maxB.Length; j++)
+                {
+                    if (temp > input[i, j])
+                    {
+                        temp = input[i, j];
+                    }
+                }
+                minA[i] = temp;
+                temp = 99;
+
+            }
+
+            temp = 0;
+            for (int i = 0; i < maxB.Length; i++)
+            {
+                for (int j = 0; j < minA.Length; j++)
+                {
+                    if (temp < input[j, i])
+                    {
+                        temp = input[j, i];
+                    }
+                }
+                maxB[i] = temp;
+                temp = 0;
+            }
+
+            int maxInMinA = minA.Max();
+            int indexMaxA = Array.IndexOf(minA, maxInMinA);
+            
+            int minInMaxB = maxB.Min();
+            int indexMinB = Array.IndexOf(maxB, minInMaxB);
+            
+            // test Print
+
+            for (int i = 0; i < minA.Length; i++)
+            {
+                for (int j = 0; j < maxB.Length; j++)
+                {
+                    Console.Write(input[i, j] + "\t");
+                }
+                Console.WriteLine("| "+minA[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < maxB.Length; i++)
+            {
+                Console.Write(maxB[i] + "\t");
+            }
+            Console.WriteLine();
+
+            // End
+
+            Console.WriteLine();
+            Console.WriteLine("Нижня ціна гри, гравця А= " + maxInMinA + "\t із стратегії А" + (indexMaxA+1));
+            Console.WriteLine("Верхня ціна гри, гравця B= " + minInMaxB + "\t із стратегії B" + (indexMinB+1));
+            Console.WriteLine();
+            if (maxInMinA != minInMaxB)
+                Console.WriteLine("Оскільки визначені стратегії гравців не рівні, то сідлова точка не існує.\n" +
+                    "Відповідно гра не має рішення в чистих стратегіях");
+            else
+            {
+                Console.WriteLine("Проводимо пошук чистих стратегій.");
+                // Тут має викликатися функція для пошуку чистих стратегій , але оскільк у мене не інує сідлової точки , реалізовувати її не буду.
+            } 
+        }
+
+        public static void searchOptimalStrategy(int[,] input){
+
+        }
+}
+}
